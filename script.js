b9add52a4300c7e5c400ad44092a82953b4284b4
spicyshibe <r.rasydan.w@gmail.com>
2026-03-15 02:41:29 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// 1. ini buat nampilin tanggal
+let waktuSekarang = new Date();
+document.getElementById("tanggal-hari-ini").innerHTML = "Waktu Saat ini: " + waktuSekarang;
+
+function tampilAlert()
+{
+    alert("WOW APA INI??? WOW KEREN SEKALI BUKAN??")
+}
+
+console.log("file script.js berhasil dimuat!")
